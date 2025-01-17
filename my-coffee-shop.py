943e6e5a2ff813ef3fd2b6b3d92dad63613bf069
MZ <111mz1121@gmail.com>
2025-01-17 07:38:48 -0800

    Add files via upload

---
+++ my-coffee-shop.py
@@ -0,0 +1,25 @@
+# my barista! robot
+
+print('''hello welcome to mohamed coffee \n''')
+
+
+name = input("what is your name \n")
+
+
+
+print("hello... " + name )
+
+menu = "black coffe. " + "Flat white. " + "Cappuccino. " + "Latte.. \n\n"
+
+print("woud you like the menu \n")
+
+
+input("yes/no \n")
+
+print(menu)
+print("\n")
+print(input("woud you like to oredar \n\n"))
+
+input("yes/no \n")
+
+print("your oredar will be ready in a moment")
